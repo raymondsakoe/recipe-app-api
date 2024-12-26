cdbaf9c676de6bbb8e917587932301a1c81956d9
Raymond Sakoe <raymond.sakoe@hotmail.com>
2024-12-26 15:24:10 -0500

    user db model validation

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,6 +8,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
@@ -30,6 +31,7 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
+
 
 class UserTests(SimpleTestCase):
     """Test user creation."""
